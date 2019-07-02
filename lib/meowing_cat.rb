30e5c637a1d6e0e4ed62606f5be989a514146c9a
Rene Cocom <fasnimo@gmail.com>
2019-07-02 23:27:23 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,17 +1,17 @@
 ## code your solution here.
 class Cat
-def name=(cats_name)
-  @name = cats_name
+# def name=(cats_name)
+#   @name = cats_name
+#
+# end
+# def name
+#   @name
+# end
+#
+# def meow
+#   puts "meow!"
+# end
 
-end
-def name
-  @name
-end
-
-def meow
-  puts "meow!"
-end
-
-# attr_writer :name
-# attr_reader :name
+attr_writer :name
+attr_reader :name
 end
