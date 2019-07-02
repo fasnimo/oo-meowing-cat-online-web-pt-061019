e44d9a910bf2ddbf6ff2d96c659645b29eff9069
Rene Cocom <fasnimo@gmail.com>
2019-07-02 23:22:58 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,10 +1,12 @@
 ## code your solution here.
 class Cat
-def instantiates(cat)
-  attr_setter = cat
-end
-
-def name=(cats_name)
-  @name = cats_name
-end
+# def instantiates(cat)
+#   attr_setter = cat
+# end
+#
+# def name=(cats_name)
+#   @name = cats_name
+# end
+attr_writer :name
+attr_reader :name
 end
